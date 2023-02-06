9e3d85d8fed5ea8ea2d65448f3b8e6ffe13be20c
Yash Patil <yash.s.patil125@gmail.com>
2023-02-06 04:52:47 -0800

    update website

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { Carousel } from 'react-responsive-carousel';
 import { TwitterVideoEmbed } from 'react-twitter-embed';
 import { SocialIcon } from 'react-social-icons';
 
+import { AiFillGithub, AiOutlineTwitter, AiFillLinkedin } from "react-icons/ai";
+
 const projects = [
   {
     name: 'Helping Hands',
@@ -23,12 +25,12 @@ const projects = [
         "Helping Hands is a nonprofit tech platform that streamlines how food banks and social service organizations deliver food. Organizations can schedule deliveries to be fulfilled by volunteers and ride-share drivers en masse.",
         "As of 05/01/2021, we've delivered over 700,000 meals through our platform and partnered with logistics providers like Uber, Lyft, and Axlehire to deliver food at scale.",
         "Helping Hands has been featured in \
-          <a target='_blank' class='text-black' href='https://cbsaustin.com/news/local/two-austin-brothers-develop-program-to-connect-the-most-vulnerable-with-volunteers-to-help'>CBS</a>, \
-          <a target='_blank' class='text-black' href='https://www.fox7austin.com/news/austin-brothers-help-build-website-to-connect-immunocompromised-with-volunteers'>FOX</a>, \
-          <a target='_blank' class='text-black' href='https://www.mercurynews.com/2020/04/17/coronavirus-bay-area-seniors-vulnerable-people-can-get-help-with-shopping-errands/'>The Mercury News</a>, \
-          <a target='_blank' class='text-black' href='https://www.forbes.com/sites/chelseadavis/2020/04/15/helping-hands-the-bay-areas-grassroots-response-to-covid-19/?sh=620bbedc1951'>Forbes</a>, \
-          <a target='_blank' class='text-black' href='https://www.entrepreneur.com/video/350031'>Entrepreneur</a>, \
-          <a target='_blank' class='text-black' href='https://www.nbclosangeles.com/news/sports/dodgers-deliver-over-22000-meals-to-families-in-inglewood/2404188/'>NBC</a>, \
+          <a target='_blank' class='text-black underline' href='https://cbsaustin.com/news/local/two-austin-brothers-develop-program-to-connect-the-most-vulnerable-with-volunteers-to-help'>CBS</a>, \
+          <a target='_blank' class='text-black underline' href='https://www.fox7austin.com/news/austin-brothers-help-build-website-to-connect-immunocompromised-with-volunteers'>FOX</a>, \
+          <a target='_blank' class='text-black underline' href='https://www.mercurynews.com/2020/04/17/coronavirus-bay-area-seniors-vulnerable-people-can-get-help-with-shopping-errands/'>The Mercury News</a>, \
+          <a target='_blank' class='text-black underline' href='https://www.forbes.com/sites/chelseadavis/2020/04/15/helping-hands-the-bay-areas-grassroots-response-to-covid-19/?sh=620bbedc1951'>Forbes</a>, \
+          <a target='_blank' class='text-black underline' href='https://www.entrepreneur.com/video/350031'>Entrepreneur</a>, \
+          <a target='_blank' class='text-black underline' href='https://www.nbclosangeles.com/news/sports/dodgers-deliver-over-22000-meals-to-families-in-inglewood/2404188/'>NBC</a>, \
           and more."
       ],
       logo: "./assets/images/helping_hands_photos/hh_logo.png",
@@ -45,7 +47,7 @@ const projects = [
     subtitle: 'A machine that makes bubble tea',
     descriptions: [
     ],
-    color: "bg-blue-500",
+    color: "bg-blue-700",
     span: "col-span-4",
     show: false,
     details: {
@@ -339,7 +341,7 @@ const projects = [
     subtitle: 'Search Organic Chemistry Challenge Problems',
     descriptions: [
     ],
-    color: "bg-blue-500",
+    color: "bg-blue-700",
     span: "col-span-4",
     show: false,
     details: {
@@ -384,18 +386,8 @@ function App() {
   const showMore = (index) => {
     let temp = projectsList;
 
-    if (previouslyViewedProjectIndex != null) {
-      temp[previouslyViewedProjectIndex].show = false;
-      temp[previouslyViewedProjectIndex].span = temp[previouslyViewedProjectIndex].prev_span;
-      delete temp[previouslyViewedProjectIndex].prev_span;
-    }
+    temp[index].show = !temp[index].show;
 
-    temp[index].prev_span = temp[index].span;
-    temp[index].span = "col-span-12";
-    temp[index].show = true;
-    window.location = `#${temp[index].id}`;
-
-    setPreviouslyViewedProjectIndex(index);
     setProjects(temp);
 
     forceUpdate();
@@ -415,63 +407,62 @@ function App() {
     });
 
   return (
-    <div className="pt-20 pb-20 bg-slate-900 max-h-screen flex overflow-y-scroll justify-center">
+    <div className="pt-20 bg-white pb-20 max-h-screen flex overflow-y-scroll justify-center">
       <div className="max-w-screen-lg p-8">
         <div className='relative'>
-          <img src={require("./assets/profile_photo.jpeg")} className="w-20 rounded-full absolute right-5 -top-5 border-2 border-slate-800"></img>
-          <h1 className='font-semibold text-4xl text-slate-50'>Yash Patil</h1>
-          <p className='text-lg text-slate-50 font-normal pt-12'>Hi there! I'm Yash, a sophomore at <span className='text-red-400'>Stanford</span> studying Computer Science.</p>
-          <p className='text-lg text-slate-50 font-normal pt-4'>Currently, I'm building human in the loop ML based optimizations for ecommerce storefronts. I'm also co-directing <a className="text-teal-400" href="https://www.treehacks.com/" target="_blank">TreeHacks 2023</a>, Stanford's hackathon and one of the largest in the world.</p>
-
-          <p className='text-lg text-slate-50 font-normal pt-4'>In the past I've interned as a fullstack engineer at <a className="text-teal-400" href="https://actively.ai" target="_blank">Actively.ai</a> building software that enables every knowledge worker to be a data scientist. I've also worked as a software engineering intern at <a target='_blank' className="text-teal-400" href="https://bit.io">bit.io</a> and <a className="text-teal-400" href="https://getpei.com">Pei</a> as well as a grid modeling researcher at the <a className="text-teal-400" href="http://www.webberenergygroup.com/people/yash-patil/">Webber Energy Group</a> at UT Austin.</p>
-          <p className='text-lg text-slate-50 font-normal pt-4 pb-8'>During the pandemic, I also co-founded <a target='_blank' className="text-teal-400" href="https://helpinghands.community">Helping Hands</a>, a startup that streamlines how food banks and social service organizations deliver food at scale (over 2,000,000 meals delivered).</p>
-          <SocialIcon network="twitter" url="https://twitter.com/ypatil125" fgColor="#ffffff" className="mr-4" />
-          <SocialIcon network="github" url="https://github.com/ypat125" fgColor="#ffffff" className="mr-4" />
-          <SocialIcon network="linkedin" url="https://www.linkedin.com/in/yash-s-patil" fgColor="#ffffff" className="mr-4" />
-          <span className='text-lg text-slate-50 font-normal'>[<a target="_blank" href="./Resume.pdf" className="text-orange-400">Resume</a>]</span>
+          <h1 className='text-3xl'>Yash Patil</h1>
+          <p className='text-md font-normal pt-8'>Hi! I'm a sophomore at Stanford studying Computer Science - Systems. I also co-direct <a className="text-blue-700 underline" href="https://www.treehacks.com/" target="_blank">TreeHacks</a>, Stanford's annual hackathon and one of the largest in the world.</p>
+          <p className='text-md font-normal pt-4'>I spent last summer in NYC building human in the loop ML based optimizations for ecommerce storefronts with my best friend <a className="text-blue-700 underline" href="https://longo.land/" target="_blank">Daniel</a>. </p>
+          <p className='text-md font-normal pt-4'>In the past I've interned as a fullstack engineer at <a className="text-blue-700 underline" href="https://actively.ai" target="_blank">Actively.ai</a> building software that enables every knowledge worker to be a data scientist. I've also worked as a software engineering intern at <a target='_blank' className="text-blue-700 underline" href="https://bit.io">bit.io</a> and <a className="text-blue-700 underline" href="https://getpei.com">Pei</a> as well as a grid modeling researcher at the <a className="text-blue-700 underline" href="http://www.webberenergygroup.com/people/yash-patil/">Webber Energy Group</a> at UT Austin.</p>
+          <p className='text-md font-normal pt-4 pb-8'>During the pandemic, I also co-founded <a target='_blank' className="text-blue-700 underline" href="https://helpinghands.community">Helping Hands</a>, a startup that streamlines how food banks and social service organizations deliver food at scale (over 2,000,000 meals delivered).</p>
+          <div className='flex flex-row items-center'>
+            <a href="https://twitter.com/ypatil125" target="_blank"><AiOutlineTwitter className="mr-4" size={30} /></a>
+            <a href="https://github.com/ypat125" target="_blank"><AiFillGithub className="mr-4" size={30} /></a>
+            <a href="https://www.linkedin.com/in/yash-s-patil" target="_blank"><AiFillLinkedin className="mr-4" size={30} /></a>
+            <span className='text-md font-normal text-blue-700 underline mr-4'>[<a href="https://yash.posthaven.com/">Writing</a>]</span>
+            <span className='text-md font-normal text-blue-700 underline'>[<a target="_blank" href="./Resume.pdf">Resume</a>]</span>
+          </div>
         </div>
 
-        <h2 className='font-semibold text-2xl text-slate-50 pt-16 pb-4'>Projects <span className="text-slate-400">(click on each to expand)</span></h2>
-        <div className="grid max-w-full sm:grid-cols-12 md:grid-cols-12 lg:grid-cols-12 gap-4">
-          {projectsList.map((project, index) =>
-            <div id={project.id} className={`${isMobile ? 'col-span-12' : project.span} ${project.color} ${project.show ? 'p-8' : 'p-4'} ${project.show ? 'ring-offset-1 ring-2 ring-white' : ''} flex cursor-pointer flex-col shadow-2xl rounded-sm`}
-              onClick={() => showMore(index)}>
-              {project.show && project.details.logo && <div>
-                <img src={require(`${project.details.logo}`)} className="w-20 rounded-full bg-white p-4 mb-6"></img>
-              </div>}
-              <div className={`${project.show ? 'text-2xl' : 'text-lg'} font-semibold`}>
-                <span className={`text-slate-50`}>{project.name}</span><span className='text-slate-50'> — {project.subtitle}</span>
-              </div>
-              {project.show && <div>
-                <a target='_blank' href={project.details.link} className={`text-slate-50 text-lg underline`}>{project.details.link_title}</a>
-                {project.details.descriptions.map((description) =>
-                  <p className='text-lg text-slate-50 font-normal pt-4' dangerouslySetInnerHTML={{ __html: description }}></p>
-                )}
-                <div className={`grid ${project.details.image_display} gap-4 mt-4`}>
-                  {project.details.images.map((image) =>
-                    <img src={require(`${image}`)} className="h-100 rounded-md"></img>
-                  )}
-                </div>
-                {project.details.gif && <img src={project.details.gif} className="w-full rounded-md"></img>}
-                {project.details.boba &&
-                  <div className="w-full justify-center align-middle text-center">
-                    <TwitterVideoEmbed id={'1503571971249893378'} />
+        <div id='projects' className='flex flex-col mt-12 pb-12'>
+          <h2 className='text-lg'>Projects</h2>
+          <ul>
+            {projectsList.map((project, index) =>
+              <li>
+                {"- "}<span className='text-sm'>[<a onClick={() => showMore(index)} className='text-rose-500 cursor-pointer underline'>info</a>]</span>{" "}<a className='text-blue-700 underline' href={project.details.link} target="_blank">{project.name}</a> {" - "}{project.subtitle}
+                {project.show && <div className='border border-black p-4 my-4'>
+                  {project.show && project.details.logo && <div>
+                    <img src={require(`${project.details.logo}`)} className="w-20 rounded-full bg-white p-4"></img>
                   </div>}
-                {project.details.scio && <Carousel renderItem={customRenderItem} renderThumbs={customRenderThumb}>
-                  <YoutubeSlide key="mission-possible" url="https://www.youtube.com/embed/EhfatoRAiEc" />
-                  <YoutubeSlide key="mousetrap-vehicle" url="https://www.youtube.com/embed/ZkYCkzl9gTA" />
-                  <YoutubeSlide key="air-trajectory" url="https://www.youtube.com/embed/pJ17PN2uRyw" />
-                  <YoutubeSlide key="scrambler" url="https://www.youtube.com/embed/eRRIGAWF3BQ" />
-                  <YoutubeSlide key="ping-pong-parachute" url="https://www.youtube.com/embed/msWRiuodZIg" />
-                  <YoutubeSlide key="gravity-vehicle" url="https://www.youtube.com/embed/pJkYwY7tObI" />
-                </Carousel>}
-              </div>}
-            </div>
-          )}
+                  {project.show && <div>
+                    <a target='_blank' href={project.details.link} className={` text-sm text-black underline`}>{project.details.link_title}</a>
+                    {project.details.descriptions.map((description) =>
+                      <p className='text-sm text-black font-normal pt-4' dangerouslySetInnerHTML={{ __html: description }}></p>
+                    )}
+                    <div className={`grid ${project.details.image_display} gap-4 mt-4`}>
+                      {project.details.images.map((image) =>
+                        <img src={require(`${image}`)} className="h-100 rounded-md"></img>
+                      )}
+                    </div>
+                    {project.details.gif && <img src={project.details.gif} className="w-full rounded-md"></img>}
+                    {project.details.boba &&
+                      <div className="w-full justify-center align-middle text-center">
+                        <TwitterVideoEmbed id={'1503571971249893378'} />
+                      </div>}
+                    {project.details.scio && <Carousel renderItem={customRenderItem} renderThumbs={customRenderThumb}>
+                      <YoutubeSlide key="mission-possible" url="https://www.youtube.com/embed/EhfatoRAiEc" />
+                      <YoutubeSlide key="mousetrap-vehicle" url="https://www.youtube.com/embed/ZkYCkzl9gTA" />
+                      <YoutubeSlide key="air-trajectory" url="https://www.youtube.com/embed/pJ17PN2uRyw" />
+                      <YoutubeSlide key="scrambler" url="https://www.youtube.com/embed/eRRIGAWF3BQ" />
+                      <YoutubeSlide key="ping-pong-parachute" url="https://www.youtube.com/embed/msWRiuodZIg" />
+                      <YoutubeSlide key="gravity-vehicle" url="https://www.youtube.com/embed/pJkYwY7tObI" />
+                    </Carousel>}
+                  </div>}
+                </div>}
+              </li>
+            )}
+          </ul>
         </div>
-        <br />
-        <br />
-        <br />
       </div>
     </div >
   );
